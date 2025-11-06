60bec11c89d04b530294ec293c372eabaf003947
MadhankumarAI <jaip90080@gmail.com>
2025-11-06 14:47:03 +0530

    register buttton update

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -101,7 +101,7 @@ const Events = () => {
                 style={{ backgroundImage: `url(${ele.img_path})` }}
               >
                 <div className="content">
-                  <Link to={`/events/${ele.event_id}`}>Explore More</Link>
+                  <Link to={`/events/${ele.event_id}`}>Register here !</Link>
                 </div>
               </li>
             ))}
